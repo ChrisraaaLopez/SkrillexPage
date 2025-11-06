c69901f11d23cb9eb3160e01809b32f8ac03d111
ChrisraaaLopez <chrisraaalopez14@gmail.com>
2025-11-06 01:10:03 -0600

    Correccion Vite

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,6 @@ export default defineConfig({
     plugins: [
         laravel({
             input: [
-                'resources/css/app.css', 'resources/js/app.js',
                 'resources/css/estilo_index.css', 'resources/css/estilo_galeria.css', 'resources/css/estilo_contacto.css',
                 'resources/js/script_index.js', 'resources/js/script_contacto.js'
             ],
